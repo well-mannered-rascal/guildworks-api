3d9caa36f9b41fac587bf3969228275b57e3a3c0
Tom Amaral <thomasamaral2016@gmail.com>
2022-11-20 14:03:17 -0700

    Basic DBManager constructor

--- guildworks/db/manager.py
+++ guildworks/db/manager.py
@@ -1,16 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import text
+from sqlalchemy.orm import sessionmaker
+
+from models import Base
 
 
-Base = declarative_base()
+class DBManager:
+    def __init__(self, connection):
+        self.connection = connection
+        self.engine = create_engine(connection, echo=True)
+        self.sessionmaker = sessionmaker(bind=self.engine)
 
-
-if __name__ == '__main__':
-    engine = create_engine(
-        """postgresql://postgres:guildworkspass@localhost:5432
-        /guildworksdb""",
-        echo=True)
-    with engine.connect() as conn:
-        result = conn.execute(text("select current_database()"))
-        print(result.all())
+        Base.metadata.create_all(bind=self.engine)
+        self.engine.connect()
