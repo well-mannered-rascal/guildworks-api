a67e5caf164c184453b206725add376cfdac98a3
Tom Amaral <thomasamaral2016@gmail.com>
2022-11-20 14:03:36 -0700

    Consolidate models into one module

---
+++ guildworks/db/models.py
@@ -0,0 +1,53 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Numeric, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class TestModel(Base):
+    __tablename__ = "test_model"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __repr__(self):
+        return "TestModel: id='%i' name='%s'" % (self.id, self.name)
+
+
+# class Adventurer(Base):
+#     __tablename__ = "adventurer"
+
+#     id = Column(Integer, primary_key=True)
+#     first_name = Column(String)
+#     last_name = Column(String)
+#     role = Column(String)
+#     # rank TODO calculate this value based on rank points
+#     rank_points = Column(Integer)
+#     career_earnings = Column(Numeric)
+#     party_id = Column(Integer, ForeignKey("party.id"))
+#     party = relationship("Party", back_populates="adventurer")
+
+
+# class Party(Base):
+#     __tablename__ = "party"
+
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String)
+#     leader = Column(Integer, ForeignKey("adventurer.id"))
+#     members = relationship("Adventurer", back_populates="party")
+#     # rank TODO Add calculated rank based on averaging of members rank
+
+
+# class Quest(Base):
+#     __tablename__ = "quest"
+
+#     id = Column(Integer, primary_key=True)
+#     type = Column(String)  # TODO define dedicated table for quest types
+#     title = Column(String)
+#     required_rank = Column(String)
+#     reward = Column(String)
+#     rank_reward = Column(String)
+#     status = Column(String)  # TODO define dedicated table for status types
+#     assigned_party = Column(Integer, ForeignKey("party.id"))
+#     expiration = Column(DateTime)
