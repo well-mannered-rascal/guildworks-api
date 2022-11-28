1a77ab83f206de182422ab751ead4e0ebf8559ec
Tom Amaral <thomasamaral2016@gmail.com>
2022-11-27 20:26:09 -0700

    Return all quest records on get

--- guildworks/resource/quests.py
+++ guildworks/resource/quests.py
@@ -1,10 +1,19 @@
 import falcon
+from ..db import DBManager
+from ..db.models import Quest
 
 
 class QuestsResource:
-    def on_get(self, req, resp):
-        """ Return only open and ongoing quests by default """
-        print(req.params)
+    def on_get(self, req: falcon.Request, resp: falcon.Response):
+        """Return only open and ongoing quests by default"""
+
+        result = []
+        with DBManager.sessionmaker() as session:
+            row: Quest
+            for row in session.query(Quest).all():
+                result.append(row.as_dict())
+
+        resp.media = result
         resp.status = falcon.HTTP_200
 
     def on_post(self, req, resp):
