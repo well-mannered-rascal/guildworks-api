dcb76d69fceb23e5620d6c3c3d721ee35b1c95c1
Tom Amaral <thomasamaral2016@gmail.com>
2022-11-27 20:26:51 -0700

    Add generic bulk insert method to manager

--- guildworks/db/manager.py
+++ guildworks/db/manager.py
@@ -1,14 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
 from .models import Base
 
 
 class DBManager:
-    def __init__(self, connection):
+    def __init__(self, connection: str):
         self.connection = connection
         self.engine = create_engine(connection, echo=True)
         self.sessionmaker = sessionmaker(bind=self.engine)
 
         Base.metadata.create_all(bind=self.engine)
         self.engine.connect()
+
+    def insert(self, records: list[Base]):
+        with self.sessionmaker() as session:
+            for record in records:
+                session.add(record)
+            session.commit()
